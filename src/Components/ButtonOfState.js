31d0ad5227ed87e673e970a56b88a7de7f5c776c
David Casas <cem20903@gmail.com>
2022-08-20 10:38:21 +0200

    Suggests

--- src/Components/ButtonOfState.js
+++ src/Components/ButtonOfState.js
@@ -1,11 +1,29 @@
+// Llevarte este button a un component, tal y como lo has montado nunca seria reutilizable. Imagina que montamos ahora una lista de empresas y queremos que el boton al hacer click nos salga un modal.
+// Este boton no podria hacer eso, tendriamos que tener otro button
+// A la hora de componetizar, rollo atomic design, el button solo debe hacer una cosa: Click, y lo que luego paso queda en manos del componente que ha llamado el componente button
+
 function ButtonOfState({ workerList, setWorkerList, workerDetail }) {
   function WorkerStateSwitcher(workerList, setWorkerList, workerDetail) {
+    // Otra solución:
+    // let workerListTemplate = workerList.map((worker) => {
+    //   return {
+    //     ...worker,
+    //     active: worker.email !== "ulises@gmail",
+    //   };
+    // });
+
+    // Te he dado la turra por whatsapp, esto no hace una copia
     let workerListTemplate = workerList;
 
+    // workerDetail.active === false
+    // SI HACEMOS === false o === true no es bien. En este caso:
+    // !workerDetail.active
     if (workerDetail.active === false) {
       workerListTemplate[2].active = true;
     }
 
+    // Solucion1:
+    // setWorkerList([...workerListTemplate]);
     setWorkerList(workerListTemplate);
   }
 
--- src/Components/ListOfWorkerCards.js
+++ src/Components/ListOfWorkerCards.js
@@ -1,97 +1,87 @@
 import { useEffect, useState } from "react";
 import WorkerPreviewCard from "./WorkerPreviewCard";
 
-
 function ListOfWorkers({ setWorkerDetail }) {
+  const [workerList, setWorkerList] = useState([]);
 
-    const [workerList, setWorkerList] = useState([]);
+  useEffect(() => {
+    setWorkerList([
+      {
+        name: "Agamenón",
+        email: "agamenón@gmail",
+        active: true,
+      },
+      {
+        name: "Aquiles",
+        email: "aquiles@gmail",
+        active: true,
+      },
+      {
+        name: "Ulises",
+        email: "ulises@gmail",
+        active: false,
+      },
+    ]);
+  }, []);
+  //¿Por qué cuando doy al botón en la pantalla no me vuelve a cargar el componente ListOfWorkers con Ulises ya en activo?
+  //Si nos fijamos, al dar al botón, Ulises cambia a activo y el estado "workerList" se setea; sin embargo,
+  // el componente no se re-renderiza y por tanto no aparece en activos. Si el estado "workerList" ha cambiado
+  //¿por qué no se re-renderiza el componente "ListOfWorkers"?
 
-    useEffect(() => {
+  console.log("SOY LISTOFWORKERCARDS:", workerList);
 
-        setWorkerList([{
+  return (
+    //¿Cómo aplico DRY en este Return?
+    // Si vas a utilizar la misma lista si o si tendras que iterarlo 2 veces, no hay mucho que rascar ahi
+    <>
+      <div className="active-workers">
+        ACTIVE WORKERS
+        {workerList.map((worker, position) => {
+          // Buen intento pero realmente no crees que worker.active ya te describe el estado del worker?
+          // Si haces un === true es sintoma de que algo esta regular
+          // No esta mal, pero en todo caso hubiera puesto  const WORKER_IS_ACTIVE = worker.active
+          const WORKER_IS_ACTIVE = worker.active === true;
 
-            name: "Agamenón",
-            email: "agamenón@gmail",
-            active: true
-        },
-        {
-            name: "Aquiles",
-            email: "aquiles@gmail",
-            active: true
-        },
-        {
-            name: "Ulises",
-            email: "ulises@gmail",
-            active: false
-        }])
-
-    }, []);
-    //¿Por qué cuando doy al botón en la pantalla no me vuelve a cargar el componente ListOfWorkers con Ulises ya en activo? 
-    //Si nos fijamos, al dar al botón, Ulises cambia a activo y el estado "workerList" se setea; sin embargo, 
-    // el componente no se re-renderiza y por tanto no aparece en activos. Si el estado "workerList" ha cambiado 
-    //¿por qué no se re-renderiza el componente "ListOfWorkers"?
-
-
-    console.log("SOY LISTOFWORKERCARDS:", workerList)
-
-
-    return (
-        //¿Cómo aplico DRY en este Return?
-        <>
-            <div className="active-workers">ACTIVE WORKERS
-                {
-                    workerList.map((worker, position) => {
-
-                        const WORKER_IS_ACTIVE = worker.active === true
-
-                        return (
-
-                            <div key={position}>
-                                {WORKER_IS_ACTIVE && <WorkerPreviewCard
-                                    key={position}
-                                    setWorkerDetail={setWorkerDetail}
-                                    worker={worker}
-                                    workerList={workerList}
-                                    setWorkerList={setWorkerList}
-                                />}
-                            </div>
-
-
-
-
-                        )
-                    })
-                }
+          return (
+            <div key={position}>
+              {WORKER_IS_ACTIVE && (
+                <WorkerPreviewCard
+                  key={position}
+                  setWorkerDetail={setWorkerDetail}
+                  worker={worker}
+                  workerList={workerList}
+                  setWorkerList={setWorkerList}
+                />
+              )}
             </div>
+          );
+        })}
+      </div>
 
-            <div className="non-active-workers">NON-ACTIVE WORKERS
-                {
-                    workerList.map((worker, position) => {
-
-                        const WORKER_IS_NOT_ACTIVE = worker.active === false
-                        return (
-
-                            <div key={position}>
-                                {WORKER_IS_NOT_ACTIVE && <WorkerPreviewCard
-                                    key={position}
-                                    setWorkerDetail={setWorkerDetail}
-                                    worker={worker}
-                                    workerList={workerList}
-                                    setWorkerList={setWorkerList}
-                                />}
-                            </div>
-
-
-                        )
-                    })
-                }
+      <div className="non-active-workers">
+        {/* Los textos dentro de etiquetas, no los dejes flotando */}
+        {/* <h1>NON-ACTIVE WORKERS</h1> */}
+        NON-ACTIVE WORKERS
+        {workerList.map((worker, position) => {
+          // const WORKER_IS_NOT_ACTIVE = !worker.active
+          const WORKER_IS_NOT_ACTIVE = worker.active === false;
+          return (
+            <div key={position}>
+              {WORKER_IS_NOT_ACTIVE && (
+                <WorkerPreviewCard
+                  key={position}
+                  setWorkerDetail={setWorkerDetail}
+                  worker={worker}
+                  workerList={workerList}
+                  setWorkerList={setWorkerList}
+                />
+              )}
             </div>
-
-
-        </>
-    )
-
-
+          );
+        })}
+      </div>
+    </>
+  );
 }
 
-export default ListOfWorkers
+export default ListOfWorkers;
