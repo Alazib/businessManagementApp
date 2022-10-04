a8ec5044805389db2ad102c8aef9dcf5f6124ba6
J.Alberto Azevedo <alazib7@gmail.com>
2022-10-04 14:21:53 +0200

    Trying basic testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,6 @@
 import { render, screen } from "@testing-library/react"
 import Contact from "./components/Contact"
 
-// test("renders learn react link", () => {
-//   render(<App />)
-//   const linkElement = screen.getByText(/learn react/i)
-
-//   expect(linkElement).toBeInTheDocument()
-// })
-
 test("renders 'COMING SOON . . .' notice", () => {
   render(<Contact />)
   const provisionalNotice = screen.getByText(/COMING SOON . . ./i)
